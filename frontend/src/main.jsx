152c493e2e1dbaffb7e88acf043c41b3344ee0d7
AlouiLouai <louaialoui1993@gmail.com>
2023-09-25 16:20:13 +0100

    thinking about pages and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import AuthPage from "./pages/Auth.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+import ErrorPage from "./pages/Error.jsx";
+import EventsPage from "./pages/Events.jsx";
+import BookingsPage from "./pages/Bookings.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/auth",
+    element: <AuthPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/events",
+    element: <EventsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/bookings",
+    element: <BookingsPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
